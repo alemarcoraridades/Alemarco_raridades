f9128bc9c448b353606227a47e303a0bc875d51f
alemarcoraridades <alemarco.raridades@gmail.com>
2024-12-04 20:08:27 -0300

    update geracao urls

--- src/lib/quiz/urls.ts
+++ src/lib/quiz/urls.ts
@@ -1,8 +1,8 @@
 export const getQuizShareUrl = (quizId: string): string => {
-  const baseUrl = window.location.origin;
+  const baseUrl = window.location.origin + '/Alemarco_raridades';// Adiciona o subdiretorio
   return `${baseUrl}/quiz/${quizId}`;
 };
 
 export const getQuizPreviewUrl = (quizId: string): string => {
-  return `/quiz/${quizId}/preview`;
+  return `/Alemarco_raridades/quiz/${quizId}/preview`; // Ajuste para subdiretorio
 };
